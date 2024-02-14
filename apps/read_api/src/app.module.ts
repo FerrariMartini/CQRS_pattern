e2602acc5a8256b29a203ff49961b43f4d711c2f
Ivan Martini <ivan.martini@mobly.com.br>
2024-02-14 11:26:32 +0000

    create read api server and healt check route

---
+++ apps/read_api/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { HealthService } from './health/health.app.service';
+import { HealthController } from './health/health.app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const DB_CONECTION = process.env.MONGO_URI || 'mongodb://localhost/db';
+
+@Module({
+  imports: [MongooseModule.forRoot(DB_CONECTION)],
+  controllers: [HealthController],
+  providers: [HealthService],
+})
+export class AppModule {}
